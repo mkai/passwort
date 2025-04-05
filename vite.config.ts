0af6af382183bb7b0cc6bb4d47baeb8e11af78c6
Markus Kaiserswerth <github@sensun.org>
2025-04-05 16:56:13 +0100

    Add comment about loader script

--- vite.config.ts
+++ vite.config.ts
@@ -11,19 +11,22 @@ const loaderPlugin = (): Plugin => {
       const mainEntry = Object.entries(bundle).find(
         ([name]) => name.startsWith("assets/index-") && name.endsWith(".js"),
       )
-
       if (!mainEntry) {
         throw new Error("failed to generate loader: main entry not found")
       }
 
-      // Create loader script content
+      const [mainEntrySrc] = mainEntry
+
+      // Generate the content of the loader script. Uses `document.currentScript`
+      // to determine its own absolute URL dynamically at runtime, so that it can
+      // refer to the correct main entry point hosted on the same domain.
       const loaderContent = `
         (function(){
           const currentScript = document.currentScript;
           const baseUrl = currentScript ? new URL('.', currentScript.src).href : '/';
           const script = document.createElement('script');
           script.type = 'module';
-          script.src = baseUrl + '${mainEntry[0]}';
+          script.src = baseUrl + '${mainEntrySrc}';
           document.head.appendChild(script);
         })();
       `
